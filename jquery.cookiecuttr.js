ba0eb212ac5634851e085a12550da4b8c43c22d8
whereizmymind <deborah@rainbow-studio.fr>
2015-10-06 23:57:35 +0200

    Remove analytics only options Translate message in french Add trackingCookiesNames Add option for reload and implicit agreement

--- jquery.cookiecuttr.js
+++ jquery.cookiecuttr.js
@@ -26,23 +26,22 @@
         var defaults = {
             cookieCutter: false, // you'd like to enable the div/section/span etc. hide feature? change this to true
             cookieCutterDeclineOnly: false, // you'd like the CookieCutter to only hide when someone has clicked declined set this to true
-            cookieAnalytics: true, // just using a simple analytics package? change this to true
-            cookieDeclineButton: false, // this will disable non essential cookies
+            cookieDeclineButton: true, // this will disable non essential cookies
             cookieAcceptButton: true, // this will disable non essential cookies
             cookieResetButton: false,
             cookieOverlayEnabled: false, // don't want a discreet toolbar? Fine, set this to true
             cookiePolicyLink: '/privacy-policy/', // if applicable, enter the link to your privacy policy here...
-            cookieMessage: 'We use cookies on this website, you can <a href="{{cookiePolicyLink}}" title="read about our cookies">read about them here</a>. To use the website as intended please...',
-            cookieAnalyticsMessage: 'We use cookies, just to track visits to our website, we store no personal details.',
+            cookieMessage: 'Ce site utilise des cookies à des fins de mesure d\'audience, aucune information personnelle n\'est conservé. En  <a href="{{cookiePolicyLink}}" title="En savoir plus sur la politique de confidentialité du site">savoir plus</a> ou ',
+            cookieNotificationLocationBottom: true, // top or bottom - they are your only options, so true for bottom, false for top
+            trackingCookiesNames: ["__utma","__utmb","__utmc","__utmt","__utmv","__utmz","_ga","_gat"], // Cookies to kill if declined
+            cookieReload: false, // change to true to  reload the page when accepting the cookies
+            cookieImplicit: true // change to false to always display the cookie banner (explicit agreement)
             cookieErrorMessage: "We\'re sorry, this feature places cookies in your browser and has been disabled. <br>To continue using this functionality, please",
-            cookieWhatAreTheyLink: "http://www.allaboutcookies.org/",
             cookieDisable: '',
             cookieExpires: 365,
-            cookieAcceptButtonText: "ACCEPT COOKIES",
-            cookieDeclineButtonText: "DECLINE COOKIES",
+            cookieAcceptButtonText: "OK, j'ai compris",
+            cookieDeclineButtonText: "S'opposer",
             cookieResetButtonText: "RESET COOKIES FOR THIS WEBSITE",
-            cookieWhatAreLinkText: "What are cookies?",
-            cookieNotificationLocationBottom: false, // top or bottom - they are your only options, so true for bottom, false for top            
             cookiePolicyPage: false,
             cookiePolicyPageMessage: 'Please read the information below and then choose from the following options',
             cookieDiscreetLink: false,
@@ -50,31 +49,27 @@
             cookieDiscreetLinkText: "Cookies?",
             cookieDiscreetPosition: "topleft", //options: topleft, topright, bottomleft, bottomright         
             cookieNoMessage: false, // change to true hide message from all pages apart from your policy page
-            cookieDomain: ""
+            cookieDomain: "",
         };
         var options = $.extend(defaults, options);
         var message = defaults.cookieMessage.replace('{{cookiePolicyLink}}', defaults.cookiePolicyLink);
-        defaults.cookieMessage = 'We use cookies on this website, you can <a href="' + defaults.cookiePolicyLink + '" title="read about our cookies">read about them here</a>. To use the website as intended please...';
+        // defaults.cookieMessage = 'We use cookies on this website, you can <a href="' + defaults.cookiePolicyLink + '" title="read about our cookies">read about them here</a>. To use the website as intended please...';
         //convert options
         var cookiePolicyLinkIn = options.cookiePolicyLink;
         var cookieCutter = options.cookieCutter;
         var cookieCutterDeclineOnly = options.cookieCutterDeclineOnly;
-        var cookieAnalytics = options.cookieAnalytics;
         var cookieDeclineButton = options.cookieDeclineButton;
         var cookieAcceptButton = options.cookieAcceptButton;
         var cookieResetButton = options.cookieResetButton;
         var cookieOverlayEnabled = options.cookieOverlayEnabled;
         var cookiePolicyLink = options.cookiePolicyLink;
         var cookieMessage = message;
-        var cookieAnalyticsMessage = options.cookieAnalyticsMessage;
         var cookieErrorMessage = options.cookieErrorMessage;
         var cookieDisable = options.cookieDisable;
-        var cookieWhatAreTheyLink = options.cookieWhatAreTheyLink;
         var cookieExpires = options.cookieExpires;
         var cookieAcceptButtonText = options.cookieAcceptButtonText;
         var cookieDeclineButtonText = options.cookieDeclineButtonText;
         var cookieResetButtonText = options.cookieResetButtonText;
-        var cookieWhatAreLinkText = options.cookieWhatAreLinkText;
         var cookieNotificationLocationBottom = options.cookieNotificationLocationBottom;
         var cookiePolicyPage = options.cookiePolicyPage;
         var cookiePolicyPageMessage = options.cookiePolicyPageMessage;
@@ -83,6 +78,9 @@
         var cookieDiscreetLinkText = options.cookieDiscreetLinkText;
         var cookieDiscreetPosition = options.cookieDiscreetPosition;
         var cookieNoMessage = options.cookieNoMessage;
+        var trackingCookiesNames = options.trackingCookiesNames;
+        var cookieReload = options.cookieReload;
+        var cookieImplicit = options.cookieImplicit;
         // cookie identifier
         var $cookieAccepted = $.cookie('cc_cookie_accept') == "cc_cookie_accept";
         $.cookieAccepted = function () {
@@ -177,20 +175,14 @@
                     $('div.cc-cookies').css("bottom", "0");
                     $('div.cc-cookies').css("right", "0");
                 }
-            } else if (cookieAnalytics) { // show analytics overlay
-                if (appOrPre) {
-                    $('body').append('<div class="cc-cookies ' + cookieOverlay + '">' + cookieAnalyticsMessage + cookieAccept + cookieDecline + '<a href="' + cookieWhatAreTheyLink + '" title="Visit All about cookies (External link)">' + cookieWhatAreLinkText + '</a></div>');
-                } else {
-                    $('body').prepend('<div class="cc-cookies ' + cookieOverlay + '">' + cookieAnalyticsMessage + cookieAccept + cookieDecline + '<a href="' + cookieWhatAreTheyLink + '" title="Visit All about cookies (External link)">' + cookieWhatAreLinkText + '</a></div>');
-                }
-            }
+            } 
             if (cookiePolicyPage) { // show policy page overlay
                 if (appOrPre) {
                     $('body').append('<div class="cc-cookies ' + cookieOverlay + '">' + cookiePolicyPageMessage + " " + ' <a href="#accept" class="cc-cookie-accept">' + cookieAcceptButtonText + '</a> ' + ' <a href="#decline" class="cc-cookie-decline">' + cookieDeclineButtonText + '</a> ' + '</div>');
                 } else {
                     $('body').prepend('<div class="cc-cookies ' + cookieOverlay + '">' + cookiePolicyPageMessage + " " + ' <a href="#accept" class="cc-cookie-accept">' + cookieAcceptButtonText + '</a> ' + ' <a href="#decline" class="cc-cookie-decline">' + cookieDeclineButtonText + '</a> ' + '</div>');
                 }
-            } else if ((!cookieAnalytics) && (!cookieDiscreetLink)) { // show privacy policy option
+            } else if (!cookieDiscreetLink) { // show privacy policy option
                 if (appOrPre) {
                     $('body').append('<div class="cc-cookies ' + cookieOverlay + '">' + cookieMessage + cookieDecline + cookieAccept + '</div>');
                 } else {
@@ -215,6 +207,13 @@
         }
         // setting the cookies
 
+        // Implicit agreement
+        if (!$cookieAccepted && cookieImplicit) {
+            $.cookie("cc_cookie_accept", "cc_cookie_accept", {
+                expires: cookieExpires,
+                path: '/'
+            });
+        }
         // for top bar
         $('.cc-cookie-accept, .cc-cookie-decline').click(function (e) {
             e.preventDefault();
@@ -227,39 +226,13 @@
                     path: '/'
                 });
                 if (options.cookieDomain) {
-                    // kill google analytics cookies
-                    $.cookie("__utma", null, {
-                        domain: '.' + options.cookieDomain,
-                        path: '/'
-                    });
-                    $.cookie("__utmt", null, {
-                        domain: '.' + options.cookieDomain,
-                        path: '/'
-                    });
-                    $.cookie("__utmb", null, {
-                        domain: '.' + options.cookieDomain,
-                        path: '/'
-                    });
-                    $.cookie("__utmc", null, {
-                        domain: '.' + options.cookieDomain,
-                        path: '/'
-                    });
-                    $.cookie("__utmz", null, {
-                        domain: '.' + options.cookieDomain,
-                        path: '/'
-                    });
-                    $.cookie("__utmv", null, {
-                        domain: '.' + options.cookieDomain,
-                        path: '/'
-                    });
-                    $.cookie("_ga", null, {
-                        domain: '.' + options.cookieDomain,
-                        path: '/'
-                    });
-                    $.cookie("_gat", null, {
-                        domain: '.' + options.cookieDomain,
-                        path: '/'
-                    });
+                    var name;
+                    for(name in trackingCookiesNames){
+                        $.cookie(trackingCookiesNames[name], null, {
+                            domain: '.' + options.cookieDomain,
+                            path: '/'
+                        });
+                    }
                 }
             } else {
                 $.cookie("cc_cookie_decline", null, {
@@ -286,7 +259,9 @@
             });
             $(".cc-cookies").fadeOut(function () {
                 // reload page to activate cookies
-                location.reload();
+                if (cookieReload) {
+                    location.reload();
+                }
             });
         });
         //cookie error accept
